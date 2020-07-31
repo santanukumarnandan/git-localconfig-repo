92d4f989fd8e8d8347f37505096ca153595171f3
HAL02301 <HAL02301@H25254.halfordsgroup.com>
2020-07-31 12:36:06 +0100

    Currency Exchange Service

---
+++ currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -0,0 +1,19 @@
+package com.example.currencyexchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@ComponentScan("com.example")
+@EnableDiscoveryClient
+public class CurrencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+
+}
