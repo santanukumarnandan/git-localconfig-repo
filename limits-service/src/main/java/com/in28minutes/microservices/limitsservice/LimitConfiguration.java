549af9964a2b2328fbc3633fe360af36e0a7cd0d
HAL02301 <HAL02301@H25254.halfordsgroup.com>
2020-07-31 12:48:53 +0100

    Limit Service

---
+++ limits-service/src/main/java/com/in28minutes/microservices/limitsservice/LimitConfiguration.java
@@ -0,0 +1,31 @@
+package com.in28minutes.microservices.limitsservice;
+
+public class LimitConfiguration {
+
+	private int maximum;
+	private int minimum;
+	
+	protected LimitConfiguration() {
+		
+	}
+	
+	public int getMaximum() {
+		return maximum;
+	}
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+	public int getMinimum() {
+		return minimum;
+	}
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+	public LimitConfiguration(int maximum, int minimum) {
+		super();
+		this.maximum = maximum;
+		this.minimum = minimum;
+	}
+	
+	
+}
