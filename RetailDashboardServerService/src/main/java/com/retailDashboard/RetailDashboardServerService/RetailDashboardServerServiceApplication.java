96ed2f23ea9af2299dd18911dcfde165e85e6c9d
HAL02301 <HAL02301@H25254.halfordsgroup.com>
2020-08-05 15:28:41 +0100

    Retail Dashboard Server Project

---
+++ RetailDashboardServerService/src/main/java/com/retailDashboard/RetailDashboardServerService/RetailDashboardServerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.retailDashboard.RetailDashboardServerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailDashboardServerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailDashboardServerServiceApplication.class, args);
+	}
+
+}
