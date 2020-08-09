3f383127a512b14d4113c206099f7f1d21461142
HAL02301 <HAL02301@host.docker.internal>
2020-08-09 16:08:22 +0100

    Modified code on 9th Aug

--- RetailDashboardServerService/src/main/java/com/retailDashboard/RetailDashboardServerService/HhtStatusDisplay.java
+++ RetailDashboardServerService/src/main/java/com/retailDashboard/RetailDashboardServerService/HhtStatusDisplay.java
@@ -1,12 +1,17 @@
 package com.retailDashboard.RetailDashboardServerService;
 
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.springframework.stereotype.Repository;
+
 
 
 
@@ -25,8 +30,67 @@ public class HhtStatusDisplay {
 		  	hhtAttributesSecondtMap.clear();
 			hhtAttributesThirdtMap = new HashMap<String, HHTAttributeBean>();
 			hhtAttributesThirdtMap.clear();
+			hhtAttributesparentMap.put("", new HHTAttributeBean("", "", "", "", "", "", "", "", "", ""));
+			
+			for (Entry<String, HHTAttributeBean> mapElement : hhtAttributesparentMap.entrySet()) { 
+	            String ipAddress = (String)mapElement.getKey(); 
+	            HHTAttributeBean hhtDetails = ((HHTAttributeBean)mapElement.getValue()); 
+	  
+	            
+	            InetAddress geek1 = InetAddress.getByName(ipAddress);
+			    geek1.getHostAddress();
+			    geek1.getHostName();
+		//	    System.out.println("Sending Ping Request to " + ipAddress); 
+			    if(geek1.isReachable(140)) {
+			    	geek1.getHostName();
+			    //	tillAttributes.put(ipAddress, new HallfordsTillStatusAttributes(storeDetails.getStoreName(), storeDetails.getStoreNumber(),storeDetails.getTillIpAddress(), "Till is up and running", storeDetails.getStoreAddress()));
+			    }
+			    else {
+			    	hhtAttributesparentMap.put(ipAddress, new HHTAttributeBean(hhtDetails.getHhtIpAddress(), hhtDetails.getHhtStoreNumber(), hhtDetails.getHhtPollingDate(), hhtDetails.getHhtstoreAddressLink(), hhtDetails.getHhtstoreName(), hhtDetails.getHhtPollingTime(), hhtDetails.getHhtPollingNumber(), hhtDetails.getHhtDateUp(), hhtDetails.getHhtTimeUp(), hhtDetails.getHhtDownSince()));
+
+			    }
+			}
+			
+			
+			for (Entry<String, HHTAttributeBean> mapElement : hhtAttributesparentMap.entrySet()) { 
+	            String ipAddress = (String)mapElement.getKey(); 
+	            HHTAttributeBean hhtDetails = ((HHTAttributeBean)mapElement.getValue()); 
+	  
+	            
+	            InetAddress geek1 = InetAddress.getByName(ipAddress);
+			    geek1.getHostAddress();
+			    geek1.getHostName();
+		//	    System.out.println("Sending Ping Request to " + ipAddress); 
+			    if(geek1.isReachable(140)) {
+			    	geek1.getHostName();
+			    //	tillAttributes.put(ipAddress, new HallfordsTillStatusAttributes(storeDetails.getStoreName(), storeDetails.getStoreNumber(),storeDetails.getTillIpAddress(), "Till is up and running", storeDetails.getStoreAddress()));
+			    }
+			    else {
+			    	hhtAttributesSecondtMap.put(ipAddress, new HHTAttributeBean(hhtDetails.getHhtIpAddress(), hhtDetails.getHhtStoreNumber(), hhtDetails.getHhtPollingDate(), hhtDetails.getHhtstoreAddressLink(), hhtDetails.getHhtstoreName(), hhtDetails.getHhtPollingTime(), hhtDetails.getHhtPollingNumber(), hhtDetails.getHhtDateUp(), hhtDetails.getHhtTimeUp(), hhtDetails.getHhtDownSince()));
+
+			    }
+			}
 			
 		  
+			for(Entry<String, HHTAttributeBean> mapElement : hhtAttributesSecondtMap.entrySet()) {
+				for(Entry<String, HHTAttributeBean> mapElement1 : hhtAttributesparentMap.entrySet()) {
+				if(mapElement.getValue().equals(mapElement1.getValue())) {
+				
+					String ipAddress = (String)mapElement.getKey(); 
+					HHTAttributeBean hhtDetails = ((HHTAttributeBean)mapElement.getValue()); 
+			//		tillAttributes2.put(ipAddress,  new HallfordsTillStatusAttributes(storeDetails.getStoreName(), storeDetails.getStoreNumber(),storeDetails.getTillIpAddress(), "Host is  unreacheable", storeDetails.getStoreAddress()));
+					hhtAttributesparentMap.put(ipAddress, new HHTAttributeBean(hhtDetails.getHhtIpAddress(), hhtDetails.getHhtStoreNumber(), hhtDetails.getHhtPollingDate(), hhtDetails.getHhtstoreAddressLink(), hhtDetails.getHhtstoreName(), hhtDetails.getHhtPollingTime(), hhtDetails.getHhtPollingNumber(), hhtDetails.getHhtDateUp(), hhtDetails.getHhtTimeUp(), hhtDetails.getHhtDownSince()));
+				}
+			}
+				
+		      
+			
 	  }
 
+			  Collection<HHTAttributeBean> hhtCollections = hhtAttributesparentMap.values();
+		        System.out.println("Size of HHT" + hhtAttributesparentMap.size());
+		        List<HHTAttributeBean> list = new ArrayList<HHTAttributeBean>();
+		        list.addAll(hhtCollections);
+		        return list;
+		}
 }
--- RetailDashboardServerService/src/main/java/com/retailDashboard/RetailDashboardServerService/RetailDashboardServiceController.java
+++ RetailDashboardServerService/src/main/java/com/retailDashboard/RetailDashboardServerService/RetailDashboardServiceController.java
@@ -1,20 +1,33 @@
 package com.retailDashboard.RetailDashboardServerService;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.net.UnknownHostException;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 
 
+
+
 @RestController
 public class RetailDashboardServiceController {
 	
+	@Autowired
+	public HhtStatusDisplay hhtStatusDisplay;
 	
 	@GetMapping("/retail-dashboard/hhtstatus")
-	public HHTAttributeBean convertCurrency() {
+	public List<HHTAttributeBean> findHhtStatus() throws UnknownHostException, IOException {
 		
+		hhtStatusDisplay = new HhtStatusDisplay();
+	    //	System.out.println("Till Address" + displayTillStatus.getAll().get(0));
+	        List<HHTAttributeBean> hHTAttributeBean = hhtStatusDisplay.getHHTStatus();
+	     //   System.out.println("Till Address" + displayTillStatus.getAll().toString());
+	        return hHTAttributeBean;
 	}
 
 }
